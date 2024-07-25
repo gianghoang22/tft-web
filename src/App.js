6ff0ee1ad0305b925ee7bd70c92d11f911cbc3d4
Hoang Giang <133222331+gianghoang22@users.noreply.github.com>
2024-07-25 22:03:22 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import "./App.css";
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/counterSlices.js";
+import styled from "styled-components";
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const Button = styled.button({
+    background: "red",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>HOME PAGE</h1>
-      </header>
+    <div>
+      <div>
+        <Button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <span>{count}</span>
+        <Button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </div>
     </div>
   );
 }
